36dea828d409886d38b5eac281e9b50edb835bb4
milana-nayak <nayakmilana@gmail.com>
2026-01-31 19:27:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -17,12 +17,17 @@ form.addEventListener("submit", function (e) {
   }
 
   // EMAIL validation
-  if (emailInput.value.trim() === "") {
-    showError(emailInput, "Email is required");
-    isValid = false;
-  } else {
-    showSuccess(emailInput);
-  }
+  const emailPattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+
+if (emailInput.value.trim() === "") {
+  showError(emailInput, "Email is required");
+  isValid = false;
+} else if (!emailInput.value.match(emailPattern)) {
+  showError(emailInput, "Enter a valid email");
+  isValid = false;
+} else {
+  showSuccess(emailInput);
+}
 
   // PASSWORD validation
   if (passwordInput.value.length < 6) {
@@ -43,10 +48,15 @@ function showError(input, message) {
   const error = input.nextElementSibling;
   error.innerText = message;
   error.style.color = "red";
+
+  input.classList.add("error-border");
+  input.classList.remove("success-border");
 }
 
 function showSuccess(input) {
   const error = input.nextElementSibling;
   error.innerText = "";
-}
 
+  input.classList.add("success-border");
+  input.classList.remove("error-border");
+}
